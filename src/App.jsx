a482defa726fc284875350fc1830f718a3267f73
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-02-19 07:48:46 +0700

    modified:   public/site.webmanifest 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,7 @@ function App() {
   const download = async () => {
     const canvas = document.getElementsByTagName("canvas")[0];
     const link = document.createElement("a");
-    link.download = `${characters[character].name}_st.ayaka.one.png`;
+    link.download = `${characters[character].name}_sekai.ryzn.my.id.png`;
     link.href = canvas.toDataURL();
     link.click();
     await log(characters[character].id, characters[character].name, "download");
